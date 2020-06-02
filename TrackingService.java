f277435a002bf41b8dec936a6ee0d01100c5c38c
AIT <63247608+ayesit@users.noreply.github.com>
2020-06-03 01:02:09 +0530

    LocationAPI

---
+++ TrackingService.java
@@ -0,0 +1,132 @@
+package com.ait.setup;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class TrackingService extends Service {
+
+    private static final String TAG = TrackingService.class.getSimpleName();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        buildNotification();
+        loginToFirebase();
+    }
+
+    protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+//Unregister the BroadcastReceiver when the notification is tapped//
+
+            unregisterReceiver(stopReceiver);
+
+//Stop the Service//
+
+            stopSelf();
+        }
+    };
+
+    private void loginToFirebase() {
+
+//Authenticate with Firebase, using the email and password we created earlier//
+
+        String email = getString(R.string.test_email);
+        String password = getString(R.string.test_password);
+
+//Call OnCompleteListener if the user is signed in successfully//
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+
+//If the user has been authenticated...//
+
+                if (task.isSuccessful()) {
+
+//...then call requestLocationUpdates//
+
+                    requestLocationUpdates();
+                } else {
+
+//If sign in fails, then log the error//
+
+                    Log.d(TAG, "Firebase authentication failed");
+                }
+            }
+        });
+    }
+
+//Initiate the request to track the device's location//
+
+    private void requestLocationUpdates() {
+        LocationRequest request = new LocationRequest();
+
+//Specify how often your app should request the device’s location//
+
+        request.setInterval(10000);
+
+//Get the most accurate location data available//
+
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        final String path = getString(R.string.firebase_path);
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+//If the app currently has access to the location permission...//
+
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+
+//...then request location updates//
+
+            client.requestLocationUpdates(request, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+
+//Get a reference to the database, so your app can perform read and write operations//
+
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
+                    Location location = locationResult.getLastLocation();
+                    if (location != null) {
+
+//Save the location data to the database//
+
+                        ref.setValue(location);
+                    }
+                }
+            }, null);
+        }
+    }
+}
